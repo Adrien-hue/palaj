1d891af6b8f301340f8f3acde66d6674750c5bc4
Adrien-hue <adrien.houee1@gmail.com>
2026-01-27 11:13:35 +0100

    refactor(planning): remove deprecated PlanningBuilderService
    Delete unused planning_builder_service and stop exporting it from the
    application services package. Planning is now built through the agent-day
    aggregate workflow.

--- core/application/services/__init__.py
+++ core/application/services/__init__.py
@@ -5,7 +5,6 @@ from core.application.services.agent_service import AgentService
 from core.application.services.agent_planning_validator_service import AgentPlanningValidatorService
 from core.application.services.etat_jour_agent_service import EtatJourAgentService
 from core.application.services.planning.agent_planning_factory import AgentPlanningFactory
-from core.application.services.planning.planning_builder_service import PlanningBuilderService
 from core.application.services.planning.planning_day_assembler import PlanningDayAssembler
 from core.application.services.planning.poste_planning_factory import PostePlanningFactory
 from core.application.services.planning.poste_planning_day_assembler import PostePlanningDayAssembler
@@ -21,7 +20,6 @@ __all__ = [
     "AgentPlanningValidatorService",
     "EtatJourAgentService",
     "AgentPlanningFactory",
-    "PlanningBuilderService",
     "PlanningDayAssembler",
     "PostePlanningFactory",
     "PostePlanningDayAssembler",
